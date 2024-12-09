9c8ae7c78cbc19dcab1eafe88609534bcf380818
Adriano Izquierdo <128609907+AdrianoIzquierdo@users.noreply.github.com>
2024-12-09 17:06:36 -0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -15,7 +15,7 @@ def create_app():
     app = Flask(__name__)
 
     # Configuración de la base de datos
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI', 'sqlite:///pagos.db')
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI', 'postgresql+psycopg2://usuario:contraseña@host:puerto/nombre_base_datos')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     # Inicializar extensiones
